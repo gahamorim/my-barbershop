011903aa2334ba8847ea93f911d1c834cdaef93d
Gabriel Amorim <gaahamorim95@gmail.com>
2025-02-05 21:36:03 -0300

    🎉 initial: create project

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import nx from '@nx/eslint-plugin';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  eslintPluginPrettier,
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+    // Override or add rules here
+    rules: {},
+  },
+];
